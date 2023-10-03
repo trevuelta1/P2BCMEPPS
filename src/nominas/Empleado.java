f2f0055d93e45e7505a5fda563231de4117b5046
usuario <usuario@pc21-087>
2023-10-03 17:27:26 +0200

    v0.1

--- src/nominas/Empleado.java
+++ src/nominas/Empleado.java
@@ -1,5 +1,37 @@
 package nominas;
 
 public class Empleado {
-
+	public enum TipoEmpleado{Vendedor, Encargado}
+	float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		if(ventasMes < 0 || horasExtra < 0) {
+			return -1;
+		} else {
+			if(tipo == TipoEmpleado.Encargado) {
+				float total = 2500 + horasExtra * 30;
+				if(ventasMes >= 1000 && ventasMes < 1500) {
+					total = total + 100;
+				} else if(ventasMes >= 1500) {
+					total = total + 200;
+				}
+				return total;
+			} else if (tipo == TipoEmpleado.Vendedor){
+				float total = 2000 + horasExtra * 30;
+				if(ventasMes >= 1000 && ventasMes < 1500) {
+					total = total + 100;
+				} else if(ventasMes >= 1500) {
+					total = total + 200;
+				}
+				return total;
+			} else {return -1;}
+		}
+	}
+	float calculoNominaNeta(float nominaBruta) {
+		float retencion;
+		if(nominaBruta < 2100) {
+			retencion = 0;
+		} else if(nominaBruta >= 2500) {
+			retencion = nominaBruta * 0.18f;
+		} else {retencion = nominaBruta * 0.15f;}
+		return (nominaBruta - retencion);
+	}
 }
